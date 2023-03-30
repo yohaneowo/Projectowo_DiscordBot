e5e95d2fdfa68db3f90cf7dc817deeb4181f3dec
Yohanewww <yohane910@gmail.com>
2023-03-30 23:53:04 +0800

    整理

--- commands/test.js
+++ commands/test.js
@@ -12,30 +12,20 @@ module.exports = {
         const db = new sqlite3.Database("./lib/database/SQLite.db") 
         fuck();
 
-        function fuxk () {
-            return new Promise(function(resolve, reject)){
+        function fuxk() {
+            return new Promise(function (resolve, reject){
                 db.serialize(function () {
                     let guild_Ids = [];
-                    db.all('SELECT Guild_Id FROM Guild_Collection', [], function (err, rows) {\
-                        if(err){
+                    db.all('SELECT Guild_Id FROM Guild_Collection', [], function (err, rows) {
+                        if (err) {
                             reject(err)
                         } else {
-                            rows.forEach(function (row) {
-                                guild_Ids.push(row.Guild_Id)
-                                console.log(guild_Ids)
-                                console.log("hekki")
-                                console.log("hekkssi")
-
+                            const guildIds = rows.map(row => row.Guild_Id);
                         }
                     },
                     )
-                },)
-            }
+                })  
+            })
         }
-        
-        console.log(ab);
-        // Guild_Ids = db.run("SELECT Guild_Id FROM Guild_Collection")
-        // console.log(Guild_Ids);
-        // console.log(`FUCK YOU${Guild_Ids}`);
     },
-};
+}
