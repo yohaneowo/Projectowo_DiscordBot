19afcd3f7226eb697080d2d936fc4fe1eff9915b
Yohanewww <yohane910@gmail.com>
2023-04-02 04:55:15 +0800

    Fix : Server Status Done

--- commands/ServerStatus.js
+++ commands/ServerStatus.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const sqlite3 = require('sqlite3');
-const get_Guild_Ids = require('./MemberCount.js');
+const MemberCountObject = require('./MemberCount.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('server')
@@ -34,21 +34,35 @@ module.exports = {
 
         UpdateValue();
         function UpdateValue() {
-            console.log(typeof(get_Guild_Ids))
-            console.log("🚀 ~ file: ServerStatus.js:11 ~ execute ~ Guild_Id:", Guild_Id)
+            MemberCountObject.get_Guild_Ids().then(function(Guild_Ids){
+                const db = new sqlite3.Database("./lib/database/SQLite.db") 
+                        if (Guild_Ids.includes(Guild_Id.toString())) {
+                            db.serialize(function() {
+                                db.run("UPDATE Guild_Collection SET Guild_Name = ?, Owner_Id = ?, All_Members_Count = ?, Users_Count = ?, Bots_Count = ?, maximumBitrate = ?, preferredLocale = ?, createdAt = ?, premiumTier = ?, premiumSubscriptionCount = ?, nsfwLevel = ?, partnered = ? WHERE Guild_Id = ?", [Guild_Name, Owner_Id, All_Members_Count, Users_Count, Bots_Count, maximumBitrate, preferredLocale, createdAt, premiumTier, premiumSubscriptionCount, nsfwLevel, partnered, Guild_Id]),
+                                    function(err) {
+                                        if (err) {
+                                            return console.log(err.message);
+                                        } 
+                                    }  
+                            });
+                            console.log(`Updated Guild Collection Table ${Guild_Name}`)
+                        } else {
+                            db.serialize(function() {
+                                db.run("INSERT INTO Guild_Collection VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [Guild_Id, Guild_Name, Owner_Id, All_Members_Count, Users_Count, Bots_Count, maximumBitrate, preferredLocale, createdAt, premiumTier, premiumSubscriptionCount, nsfwLevel, partnered]),
+                                function(err) {
+                                    if (err) {
+                                        return console.log(err.message);
+                                    } 
+                                }
+                            });
+                            console.log(`Inserted Guild Collection Table ${Guild_Name}`)
+                        }
+                  
+            }).catch(function (err) {
+                console.error(err);
+            });
+ 
             
-            // get_Guild_Ids().then(function(guildIds){
-            //     console.log(guildIds);
-            // }).catch(function (err) {
-            //     console.error(err);
-            // });
-            const db = new sqlite3.Database("./lib/database/SQLite.db") 
-            Guild_Ids = db.run("SELECT Guild_Id FROM Guild_Collection")
-            if (Guild_Id in Guild_Ids) {
-                db.run("UPDATE Guild_Collection SET Guild_Name = ?, Owner_Id = ?, All_Members_Count = ?, Users_Count = ?, Bots_Count = ?, maximumBitrate = ?, preferredLocale = ?, createdAt = ?, premiumTier = ?, premiumSubscriptionCount = ?, nsfwLevel = ?, partnered = ? WHERE Guild_Id = ?", [Guild_Name, Owner_Id, All_Members_Count, Users_Count, Bots_Count, maximumBitrate, preferredLocale, createdAt, premiumTier, premiumSubscriptionCount, nsfwLevel, partnered, Guild_Id])
-            }  else {
-                db.run("INSERT INTO Guild_Collection VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [Guild_Id, Guild_Name, Owner_Id, All_Members_Count, Users_Count, Bots_Count, maximumBitrate, preferredLocale, createdAt, premiumTier, premiumSubscriptionCount, nsfwLevel, partnered])
-            }
     
         }
         const embed = new EmbedBuilder()
