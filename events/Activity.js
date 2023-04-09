a9d7a9f4c116d577348fe5e3e4542da637f36c89
Yohanewww <yohane910@gmail.com>
2023-04-10 02:27:59 +0800

    [Update] : Add setActivity

--- events/Activity.js
+++ events/Activity.js
@@ -1,9 +1,9 @@
 const client = require('../index.js')
+const { ActivityType } = require('discord.js');
 
-module.exports = {
-    name: 'ready',
-    once: true,
-    async execute(client) {
-        console.log(`Ready!2 LAAAAAAAAAAAAAAAAAAA ${client.user.tag}`);
-    }
+async function setActivity(client) {
+    client.user.setActivity(`with ${client.guilds.cache.reduce((a, g) => a + g.members.cache.size, 0)} members`, { type: ActivityType.Watching });
+    console.log('hi')
 }
+setInterval(() => setActivity(client), 1000 * 60 * 5 );
+
