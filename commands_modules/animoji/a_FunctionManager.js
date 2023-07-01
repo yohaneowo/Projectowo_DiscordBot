1418fc024a4f8424fe0da78e4d1cb7feafa484b5
Yohanewww <yohane910@gmail.com>
2023-07-01 08:47:47 +0800

    [Update] : Finish Animoji Emoji Pack Automize

--- commands_modules/animoji/a_FunctionManager.js
+++ commands_modules/animoji/a_FunctionManager.js
@@ -1,66 +1,68 @@
 const {
   Animoji_DatabaseFunction
-} = require('../../commands_modules/animoji/a_databaseFunctionManager.js')
+} = require("../../commands_modules/animoji/a_databaseFunctionManager.js");
+const fs = require("node:fs");
+const path = require("node:path");
 
 class Animoji_FunctionManager {
   async submitPrefix(interaction, modal) {
-    const animojiDbManager = new AnimojiDatabaseFunction()
-    const userPrefix = await animojiDbManager.getPrefix(interaction.user.id)
-    console.log(userPrefix)
+    const animojiDbManager = new Animoji_DatabaseFunction();
+    const userPrefix = await animojiDbManager.getPrefix(interaction.user.id);
+    console.log(userPrefix);
     if (userPrefix) {
-      await interaction.showModal(modal)
+      await interaction.showModal(modal);
       const filter = (i) =>
-        i.user.id === interaction.user.id && i.customId === 'modal'
+        i.user.id === interaction.user.id && i.customId === "modal";
       const submitted = await interaction
         .awaitModalSubmit({ filter, time: 60000 })
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
       if (submitted) {
-        const user_id = interaction.user.id
-        const prefix = submitted.fields.getTextInputValue('prefix')
+        const user_id = interaction.user.id;
+        const prefix = submitted.fields.getTextInputValue("prefix");
         if (prefix === userPrefix) {
           submitted.reply({
-            content: `??? Why key the same prefix you idiot`,
+            content: "??? Why key the same prefix you idiot",
             ephemeral: true
-          })
+          });
         } else {
-          await animojiDbManager.updatePrefix(user_id, prefix)
+          await animojiDbManager.updatePrefix(user_id, prefix);
           submitted.reply({
             content: `done, your prefix changed from \`${userPrefix}\` to \`${prefix}\``,
             ephemeral: true
-          })
+          });
         }
       }
     } else {
-      await interaction.showModal(modal)
+      await interaction.showModal(modal);
       const filter = (i) =>
-        i.user.id === interaction.user.id && i.customId === 'modal'
+        i.user.id === interaction.user.id && i.customId === "modal";
       const submitted = await interaction
         .awaitModalSubmit({ filter, time: 60000 })
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
 
       if (submitted) {
-        const user_id = interaction.user.id
-        const prefix = submitted.fields.getTextInputValue('prefix')
-        await animojiDbManager.insertPrefix(user_id, prefix)
+        const user_id = interaction.user.id;
+        const prefix = submitted.fields.getTextInputValue("prefix");
+        await animojiDbManager.insertPrefix(user_id, prefix);
         submitted.reply({
           content: `done, your prefix is : \`${prefix}\``,
           ephemeral: true
-        })
+        });
       }
     }
   }
   async insert_AllEmoji(interaction, guild_id, belong_to, isEmojiPack) {
     return new Promise(async (resolve, reject) => {
-      const animojiDbManager = new Animoji_DatabaseFunction()
+      const animojiDbManager = new Animoji_DatabaseFunction();
 
       await interaction.guild.emojis.cache.forEach(async (emoji) => {
-        const emoji_id = emoji.id
-        const emoji_name = emoji.name
-        const emoji_identifier = emoji.toString()
-        const createdAt = emoji.createdAt.toString()
+        const emoji_id = emoji.id;
+        const emoji_name = emoji.name;
+        const emoji_identifier = emoji.toString();
+        const createdAt = emoji.createdAt.toString();
         let animated =
-          emoji.animated == 'true' || emoji.animated == 'false' ? 1 : 0
-        const emoji_url = emoji.url
+          emoji.animated == "true" || emoji.animated == "false" ? 1 : 0;
+        const emoji_url = emoji.url;
         // console.log(`emoji_id: ${emoji_id}`)
         // console.log(`emoji_name: ${emoji_name}`)
         // console.log(`emoji_identifier: ${emoji_identifier}`)
@@ -79,16 +81,42 @@ class Animoji_FunctionManager {
             belong_to,
             emoji_url,
             isEmojiPack
-          )
-          resolve()
+          );
+          resolve();
         } catch (err) {
-          reject()
+          reject();
         }
-      })
-    })
+      });
+    });
+  }
+
+  async createGuildEmoji(interaction) {
+    const guild_name = interaction.guild.name;
+    const emojiPackPath = path.join(process.cwd(), "lib/emoji/emoji_packs");
+    // check emojipack folder name same with guild name or not
+    const emojiPackFolder = fs
+      .readdirSync(emojiPackPath)
+      .filter((file) => file.includes(guild_name));
+
+    const emojiPackFolderPath = path.join(emojiPackPath, emojiPackFolder[0]);
+    const supportedExtensions = [".png", ".jpg", ".jpeg", ".gif"];
+    const emojis = fs
+      .readdirSync(emojiPackFolderPath)
+      .filter((file) => supportedExtensions.includes(path.extname(file)));
+    console.log(`阿萨洒洒水${emojis}`);
+    for (const emoji of emojis) {
+      console.log(emoji);
+      const emoji_path = path.join(emojiPackFolderPath, emoji);
+      console.log(`LALALALA${emoji_path}`);
+      console.log(`BABABABAB${emojiPackFolderPath}/${emoji}`);
+      await interaction.guild.emojis.create({
+        attachment: `${emojiPackFolderPath}/${emoji}`,
+        name: `${emoji.replace(path.extname(emoji), "")}`
+      });
+    }
   }
 }
 
 module.exports = {
   Animoji_FunctionManager
-}
+};
--- handlers/ErrorsHandler.js
+++ handlers/ErrorsHandler.js
@@ -1,63 +1,63 @@
-const { EmbedBuilder } = require('discord.js');
-const { err_AnnouncementChannel_ID } = require('../config.json');
+const { EmbedBuilder } = require("discord.js");
+const { err_AnnouncementChannel_ID } = require("../config.json");
 module.exports = (client) => {
   const errChannel = err_AnnouncementChannel_ID;
-  process.on('unhandledRejection', (reason, promise) => {
-    console.log('[Anti-crash] :: Unhandled Rejection/Catch');
+  process.on("unhandledRejection", (reason, promise) => {
+    console.log("[Anti-crash] :: Unhandled Rejection/Catch");
     console.log(reason, promise);
     const errEmbed1 = new EmbedBuilder()
-      .setColor('#ED4245')
-      .setTitle('┏━°⌜ Error Handling System ⌟°━┓')
-      .setDescription('出大事啦')
+      .setColor("#ED4245")
+      .setTitle("┏━°⌜ Error Handling System ⌟°━┓")
+      .setDescription("出大事啦")
       .addFields({ name: `${promise}`, value: `${reason}`, inline: false })
       .setTimestamp()
-      .setFooter({ text: 'Anti Crash System' });
+      .setFooter({ text: "Anti Crash System" });
     client.channels.cache.get(errChannel)?.send({ embeds: [errEmbed1] });
   });
 
-  process.on('uncaughtException', (err, origin) => {
-    console.log('[Anti-crash] :: uncaughtException/Catch');
+  process.on("uncaughtException", (err, origin) => {
+    console.log("[Anti-crash] :: uncaughtException/Catch");
     console.log(err, origin);
     const errEmbed2 = new EmbedBuilder()
-      .setColor('#ED4245')
-      .setTitle('┏━°⌜ Error Handling System ⌟°━┓')
-      .setDescription('出大事啦')
+      .setColor("#ED4245")
+      .setTitle("┏━°⌜ Error Handling System ⌟°━┓")
+      .setDescription("出大事啦")
       .addFields({ name: `${origin}`, value: `${err}`, inline: false })
       .setTimestamp()
-      .setFooter({ text: 'Anti Crash System' });
+      .setFooter({ text: "Anti Crash System" });
     client.channels.cache.get(errChannel)?.send({ embeds: [errEmbed2] });
   });
 
-  process.on('uncaughtExceptionMonitor', (err, origin) => {
-    console.log('[Anti-crash] :: uncaughtExceptionMonitor/Catch');
+  process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log("[Anti-crash] :: uncaughtExceptionMonitor/Catch");
     console.log(err, origin);
     const errEmbed3 = new EmbedBuilder()
-      .setColor('#ED4245')
-      .setTitle('┏━°⌜ Error Handling System ⌟°━┓')
-      .setDescription('出大事啦')
+      .setColor("#ED4245")
+      .setTitle("┏━°⌜ Error Handling System ⌟°━┓")
+      .setDescription("出大事啦")
       .addFields({ name: `${origin}`, value: `${err}`, inline: false })
       .setTimestamp()
-      .setFooter({ text: 'Anti Crash System' });
+      .setFooter({ text: "Anti Crash System" });
     client.channels.cache.get(errChannel)?.send({ embeds: [errEmbed3] });
   });
 
-  process.on('syntaxError', (err) => {
+  process.on("syntaxError", (err) => {
     const errEmbed = new EmbedBuilder()
-      .setTitle('Syntax Error')
+      .setTitle("Syntax Error")
       .setDescription(err.message)
-      .addField('Stack Trace', err.stack)
+      .addField("Stack Trace", err.stack)
       .setTimestamp()
-      .setFooter({ text: 'Anti Crash System' });
+      .setFooter({ text: "Anti Crash System" });
     client.channels.cache.get(errChannel)?.send({ embeds: [errEmbed] });
   });
 
-  process.on('timeout', (err) => {
+  process.on("timeout", (err) => {
     const errEmbed = new EmbedBuilder()
-      .setTitle('Timeout Error')
+      .setTitle("Timeout Error")
       .setDescription(err.message)
-      .addField('Stack Trace', err.stack)
+      .addField("Stack Trace", err.stack)
       .setTimestamp()
-      .setFooter({ text: 'Anti Crash System' });
+      .setFooter({ text: "Anti Crash System" });
     client.channels.cache.get(errChannel).send({ embeds: [errEmbed] });
   });
 };
