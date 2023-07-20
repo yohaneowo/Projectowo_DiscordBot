018927b43a8e418f3a0a4ef3491ca92919677571
Yohanewww <yohane910@gmail.com>
2023-07-20 19:27:14 +0800

    [Update] : Add Douyin&Tiktok Api with tiktok_api_mini_Project

--- subCommands/animoji/set-guild_as_emoji_storage.js
+++ subCommands/animoji/set-guild_as_emoji_storage.js
@@ -5,6 +5,34 @@ const {
   Animoji_FunctionManager
 } = require("../../commands_modules/animoji/a_FunctionManager.js");
 module.exports = async (interaction) => {
+  const animatedEmojiCount = await interaction.guild.emojis.cache.filter(
+    (emoji) => emoji.animated
+  ).size;
+  const staticEmojiCount = await interaction.guild.emojis.cache.filter(
+    (emoji) => !emoji.animated
+  ).size;
+
+  const animatedEmojiString = await interaction.guild.emojis.cache
+    .filter((emoji) => !emoji.animated)
+    .map((emoji) => emoji.toString())
+    .join(" ");
+
+  const staticEmojiString = await interaction.guild.emojis.cache
+    .filter((emoji) => !emoji.animated)
+    .map((emoji) => emoji.toString())
+    .join(" ");
+
+  let EmojiPreview = "";
+
+  if (animatedEmojiCount > 0) {
+    EmojiPreview += `**Animated Emoji**\n ${animatedEmojiString}`;
+  }
+  if (staticEmojiCount > 0) {
+    EmojiPreview += `**Static Emoji**\n ${staticEmojiString}`;
+  }
+
+  console.log(animatedEmojiString);
+
   const animojiDbManager = new Animoji_DatabaseFunction();
   const animojiFunctionManager = new Animoji_FunctionManager();
   const owner_id = interaction.guild.ownerId;
@@ -12,9 +40,9 @@ module.exports = async (interaction) => {
   const guild_id = interaction.guild.id;
   const isEmojiPack = 0;
   if (owner_id == user_id) {
-    console.log(`interaction guild_id: ${guild_id}`);
+    // console.log(`interaction guild_id: ${guild_id}`);
     const isGuildExist = await animojiDbManager.getGuild(user_id, guild_id);
-    console.log(`isGuildExist: ${isGuildExist}`);
+    // console.log(`isGuildExist: ${isGuildExist}`);
     if (isGuildExist) {
       interaction.editReply({
         content: "This guild is already registered",
@@ -24,7 +52,7 @@ module.exports = async (interaction) => {
       const isEmojiGuildExist = await animojiDbManager.checkEmojiGuildExist(
         guild_id
       );
-      console.log(`isEmojiGuildExist: ${isEmojiGuildExist}`);
+      // console.log(`isEmojiGuildExist: ${isEmojiGuildExist}`);
       if (!isEmojiGuildExist) {
         await animojiDbManager.insertGuild(user_id, guild_id);
         try {
@@ -34,12 +62,15 @@ module.exports = async (interaction) => {
             user_id,
             isEmojiPack
           );
-          interaction.editReply({ content: "done set guild", ephemeral: true });
+          interaction.editReply({
+            content: `# Done set guild \n  animated emoji count: ${animatedEmojiCount} \n  static emoji count: ${staticEmojiCount} \n ${EmojiPreview}`,
+            ephemeral: true
+          });
         } catch (err) {
           interaction.followUp({ content: "发现有同名的哦", ephemeral: true });
         }
       } else {
-        await animojiDbManager.deleteEmoji(guild_id);
+        await animojiDbManager.delete_AllEmoji(guild_id);
         try {
           await animojiFunctionManager.insert_AllEmoji(
             interaction,
@@ -47,7 +78,10 @@ module.exports = async (interaction) => {
             user_id,
             isEmojiPack
           );
-          interaction.editReply({ content: "done set guild", ephemeral: true });
+          interaction.editReply({
+            content: `# Done set guild \n  animated emoji count: ${animatedEmojiCount} \n  static emoji count: ${staticEmojiCount}\n \n ${EmojiPreview}`,
+            ephemeral: true
+          });
         } catch (err) {
           interaction.followUp({ content: "发现有同名的哦", ephemeral: true });
         }
