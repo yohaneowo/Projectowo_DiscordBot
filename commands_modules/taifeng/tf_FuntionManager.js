6d4ebb130d3a0e351928827564a68b520e27d797
Yohanewww <yohane910@gmail.com>
2023-09-05 07:58:40 +0800

    Add taifeng.png to .gitignore and update TaiFeng_FunctionManager.js and TaiFeng.js
    - Add lib/img/api/taifeng/taifeng.png to .gitignore
    - Update getTaiFengscreenshot function name to getTaiFengScreenshot in tf_FuntionManager.js
    - Update execute function in TaiFeng.js:
      - Change command name from "test" to "台风速报"
      - Update screenshot path in getTaiFengScreenshot function call
      - Await the response of getTaiFengMessage function call
      - Extract year and ch_name from the response data
      - Check if there is a zh-hant prediction available and store it in zhHantPredictionMsg variable
      - Reply with the formatted message containing year, ch_name, and zhHantPredictionMsg

--- commands_modules/taifeng/tf_FuntionManager.js
+++ commands_modules/taifeng/tf_FuntionManager.js
@@ -1,26 +1,31 @@
 const puppeteer = require("puppeteer")
 const axios = require("axios")
-
+require("dotenv").config()
 class TaiFeng_FunctionManager {
-  async getTaiFengscreenshot(url, path) {
+  async getTaiFengScreenshot(url, path) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(url)
+    await page.goto(url, { timeout: 60000 })
     await page.screenshot({ path: path })
     await browser.close()
   }
 
   async getTaiFengMessage() {
-    axios
-      .get(
-        "https://opendata.cwb.gov.tw/api/v1/rest/datastore/W-C0034-005?Authorization=CWB-044E20C5-733F-4552-A73B-F4602A973223"
-      )
-      .then(function (response) {
-        // handle success
-        // console.log(response.data)
-        const response_data = response.data
-        return response_data
-      })
+    const apiUrl = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/W-C0034-005?Authorization=${process.env.OPENDAT_CWB_GOV_TW}`
+    // const param = {W
+    //   Authorization: process.env.OPENDAT_CWB_GOV_TW
+    // }
+    try {
+      const response = await axios.get(apiUrl)
+      // console.log(response)
+      // handle success
+      // console.log(response.data)
+      const response_data = response.data
+      // console.log(response_data)
+      return response_data
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
